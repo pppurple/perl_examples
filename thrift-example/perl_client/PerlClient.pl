d89e2bb5f3d4c5ff22ce22f2a61ef4bfb6c6aa31
pppurple <shuggie.otis0@gmail.com>
2017-12-15 12:53:35 +0900

    add thrift examples

---
+++ thrift-example/perl_client/PerlClient.pl
@@ -0,0 +1,39 @@
+#!/usr/local/bin/perl
+
+use strict;
+use warnings;
+
+use lib './lib';
+use lib '../gen-perl';
+
+use Thrift;
+use Thrift::BinaryProtocol;
+use Thrift::Socket;
+use Thrift::BufferedTransport;
+
+use exampleThrift::Types;
+use exampleThrift::PeopleService;
+
+use Data::Dumper;
+
+my $socket    = new Thrift::Socket('localhost',8080);
+my $transport = new Thrift::BufferedTransport($socket,1024,1024);
+my $protocol  = new Thrift::BinaryProtocol($transport);
+my $client    = new exampleThrift::PeopleServiceClient($protocol);
+
+eval{
+    $transport->open();
+    
+    my $response = $client->searchByName("Wall");
+    print "response: " . Dumper($response);
+
+    print "@$response[0]->{name} \n";
+    print "@$response[0]->{age} \n";
+    print "@$response[0]->{country} \n";
+    print "@$response[0]->{hobby} \n";
+
+    $transport->close();
+
+}; if($@){
+    warn(Dumper($@));
+}
---
+++ thrift-example/perl_server/PerlServer.pl
@@ -0,0 +1,66 @@
+#!/usr/local/bin/perl
+
+use strict;
+use warnings;
+
+use lib '../gen-perl';
+use lib './lib';
+
+use Thrift::Socket;
+use Thrift::Server;
+use Thrift::ServerSocket;
+
+use exampleThrift::Types;
+use exampleThrift::PeopleService;
+
+package PeopleServiceHandler;
+use base qw(exampleThrift::PeopleServiceIf);
+
+sub new {
+    my $classname = shift;
+    my $self      = {};
+
+    return bless($self,$classname);
+}
+
+
+sub searchByName{
+  my $self = shift;
+  my $query = shift;
+
+  print "query: $query \n";
+
+  my $alice = new exampleThrift::Person({name => 'Alice Wall', age => 33, country => exampleThrift::Country::JAPAN, hobby => 'trick'});
+  my $bobby = new exampleThrift::Person({name => 'Bobby Wall', age => 29, country => exampleThrift::Country::AMERICA, hobby => 'yo-yo'});
+
+  my @people;
+  push @people, $alice;
+  push @people, $bobby;
+
+  return \@people;
+}
+
+eval {
+  my $handler       = new PeopleServiceHandler;
+  my $processor     = new exampleThrift::PeopleServiceProcessor($handler);
+  my $serversocket  = new Thrift::ServerSocket(8080);
+  my $forkingserver = new Thrift::SimpleServer($processor, $serversocket);
+
+  # ForkingServer that forks a new process for each request
+  #my $forkingserver = new Thrift::ForkingServer($processor, $serversocket);
+
+  print "Starting the server... \n";
+  $forkingserver->serve();
+  print "done.\n";
+
+}; if ($@) {
+  if ($@ =~ m/TException/ and exists $@->{message}) {
+    my $message = $@->{message};
+    my $code    = $@->{code};
+    my $out     = $code . ':' . $message;
+    die $out;
+  } else {
+    die $@;
+  }
+}
+
